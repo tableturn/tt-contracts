c8e5830508117a302f43902a35b2afe9f6e17a6d
Pierre Martin <hickscorp@gmail.com>
2019-10-09 14:07:22 +0100

    Updates people address books.

--- conf/people.18021982.js
+++ conf/people.18021982.js
@@ -3,6 +3,11 @@ module.exports = {
   storage: '0xB4C87127F59c9b8653790e15F92effa62f7D17D7',
   issuer: '0xa6cD80b78638E1eCcBE933148d9BBc6AeCDc6f98',
   pk2m: '0xb1004872B989ec8894F8Dd07da85437Dff9ddb37',
-  pierre: '0xafC112c0A2a345403eB6799dAdffa752e5358550',
-  kevin: '0x6C85420646CEa01708c7881dEF18c2b1A9d239D9'
+  pierre_martin: '0xafC112c0A2a345403eB6799dAdffa752e5358550',
+  kevin_monserrat: '0x0C3461abCe09B5d1013947cC9A3D1Ea51f56F4D7',
+  radouane_oudrhiri: '',
+  martin_cornish: '0x66f37cbf52609529a32855af815d1dc8c736e6c5',
+  paul_griffith: '0xad38c6b5808a65bedc31a1c7c1519827550a042b',
+  darren_rebeiro: '0x23226e3b08aba8dca0618c1f41a77802a94a3325',
+  christophe_pechoux: '0x249ef7fee1c506d00dbb5a67f5a478717aaff12d'
 };
