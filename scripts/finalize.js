34714c265559427f2ce155f6bcd5aae1fae15358
Pierre Martin <hickscorp@gmail.com>
2019-10-18 13:16:27 +0100

    Improves finalize script and utils.

--- scripts/finalize.js
+++ scripts/finalize.js
@@ -27,6 +27,7 @@ module.exports = async done => {
     const register = await Register.at(zosAbi.proxies[`TTContracts/Register`][0].address);
     const transact = await Transact.at(zosAbi.proxies[`TTContracts/Transact`][0].address);
     const token = await Token.at(zosAbi.proxies[`TTContracts/Token`][0].address);
+    // Pause here until all awaits are fullfilled...
     const utils = generateUtils(registry, access, register, transact, token);
 
     console.log('Registering contracts with Registry...');
@@ -39,16 +40,16 @@ module.exports = async done => {
     ]);
 
     await Promise.all([
-      utils.promoteActor(people.pk2m, people.pk2m),
-      utils.promoteIssuer(people.pk2m, people.issuer)
+      utils.promoteActor(people.pk2m, governance),
+      utils.promoteIssuer(people.issuer, governance)
     ]);
 
     if (netId !== 18021982) {
       console.log('This is not production - making a few staging actors and governors...');
       await Promise.all([
-        utils.promoteGovernor(people.pk2m, people.pierre),
-        utils.promoteActor(people.pk2m, people.pierre),
-        utils.promoteActor(people.pk2m, people.kevin)
+        utils.promoteGovernor(people.pierre_martin, governance),
+        utils.promoteActor(people.pierre_martin, governance),
+        utils.promoteActor(people.kevin_monserrat, governance)
       ]);
 
       const balance = await token.balanceOf(people.pk2m);
@@ -59,9 +60,9 @@ module.exports = async done => {
         await token.allocate(people.pk2m, utils.convert('1_121_010'), governance);
 
         console.log(`Transfering tokens from ${people.pk2m} to an actor account.`);
-        await token.transfer(people.kevin, utils.convert('6050'), { from: people.pk2m });
+        await token.transfer(people.kevin_monserrat, utils.convert('6050'), { from: people.pk2m });
         console.log(`Approving transfer...`);
-        await transact.approve(people.pk2m, 0, { from: people.pierre });
+        await transact.approve(people.pk2m, 0, { from: people.pierre_martin });
       }
     }
 
--- scripts/utils.js
+++ scripts/utils.js
@@ -4,10 +4,10 @@ module.exports = (registry, access, register, transact, token) => {
     return `${clean}${'0'.repeat(6)}`;
   };
 
-  const registerContract = async (name, addr, target, governance) => {
+  const registerContract = async (name, addr, target, opts) => {
     if (addr !== target) {
       console.log(`  Setting ${name} into Registry to ${target}.`);
-      await registry[`set${name}Contract`](target, governance);
+      await registry[`set${name}Contract`](target, opts);
     } else {
       console.log(`  Registry already has the right ${name} contract.`);
     }
@@ -18,39 +18,61 @@ module.exports = (registry, access, register, transact, token) => {
     await token.issue(convert(amount), reason, issuance);
   };
 
-  const promoteIssuer = async (governor, address) => {
+  const promoteIssuer = async (address, opts = {}) => {
     if (await access.isIssuer(address)) {
       console.log(`  Address ${address} is already a governor, skipping...`);
       return;
     }
     console.log(` Making ${address} a governor.`);
-    await access.addIssuer(address, { from: governor });
+    await access.addIssuer(address, opts);
   };
 
-  const promoteGovernor = async (governor, address) => {
+  const promoteGovernor = async (address, opts = {}) => {
     if (await access.isGovernor(address)) {
       console.log(`  Address ${address} is already a governor, skipping...`);
       return;
     }
     console.log(` Making ${address} a governor.`);
-    await access.addGovernor(address, { from: governor });
+    await access.addGovernor(address, opts);
   };
 
-  const promoteActor = async (governor, address) => {
+  const promoteActor = async (address, opts = {}) => {
     if (await access.isActor(address)) {
       console.log(`  Address ${address} is already an actor, skipping...`);
       return;
     }
     console.log(`  Making ${address} an actor.`);
-    await access.addActor(address, { from: governor });
+    await access.addActor(address, opts);
+  };
+
+  const promotePk2mInvestor = async (from, to, amount) => {
+    await promoteActor(to, { from });
+    console.log(`Transfering ${amount} to ${to}...`);
+    await token.transfer(to, convert(`${amount}`), { from });
+    const id = (await transact.count(from)) - 1;
+    console.log(`Approving transfer with id ${id}...`);
+    await transact.approve(from, id, { from });
+  };
+
+  const accountancy = async people => {
+    return Object.keys(people).map(async k => [
+      k,
+      (await token.balanceOf(people[k])).toString().slice(0, -6)
+    ]);
   };
 
   return {
     convert,
     issue,
     promoteActor,
+    promotePk2mInvestor,
     promoteGovernor,
     promoteIssuer,
-    registerContract
+    registerContract,
+    accountancy
   };
 };
+
+// utils.promotePk2mInvestor(people.pk2m, people.warwick_hill, 1000)
+// utils.promotePk2mInvestor(people.pk2m, people.rhys_photis, 1000)
+// utils.promotePk2mInvestor(people.pk2m, people.jimmy_bricknell, 100)
