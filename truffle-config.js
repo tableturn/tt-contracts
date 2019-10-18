3be980a7a83053ad9d4d058ab08736e0a6ecd3c8
Pierre Martin <hickscorp@gmail.com>
2019-10-18 13:13:32 +0100

    Rollsback to a compatible version of the truffle's hdwallet.

--- truffle-config.js
+++ truffle-config.js
@@ -1,6 +1,6 @@
 require('ts-node/register');
 require('dotenv').config();
-const HDWalletProvider = require('@truffle/hdwallet-provider');
+const HDWalletProvider = require('truffle-hdwallet-provider');
 
 const hdWallet = host => {
   return new HDWalletProvider(require(`./conf/keys.${process.env.NETWORK_ID}`), host, 0, 4);
