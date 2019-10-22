3282546ae2096226a1d31e35aee9af9301cd8d01
Pierre Martin <hickscorp@gmail.com>
2019-10-22 17:23:07 +0100

    Adds pre-approval / grant functionality to Transact.

--- contracts/Transact.sol
+++ contracts/Transact.sol
@@ -15,12 +15,13 @@ contract Transact is Initializable, ITransact {
   Registry public reg;
   // Our accounts are held in this mapping.
   mapping(address => Order[]) orderBook;
+  mapping(address => Grant[]) grantBook;
 
   /// Events.
 
-  event Request(address indexed owner, uint256 id);
-  event Approval(address indexed owner, uint256 id);
-  event Rejection(address indexed owner, uint256 id);
+  event Request(address indexed owner, uint256 orderId);
+  event Approval(address indexed owner, uint256 orderId);
+  event Rejection(address indexed owner, uint256 orderId);
 
   /**
    * @dev This is the ZOS constructor.
@@ -36,7 +37,6 @@ contract Transact is Initializable, ITransact {
    * @param owner is the account from which the funds shall be frozen.
    * @param recipient is the account to which the funds would be transfered.
    * @param amount is the amount of tokens to include in the transfer.
-   * @return The transfer id that was created.
    */
   function request(
     address owner,
@@ -68,25 +68,84 @@ contract Transact is Initializable, ITransact {
     return orderBook[owner];
   }
 
-  function get(address owner, uint256 id) public view returns(Order memory) {
-    return _get(owner, id);
+  function get(address owner, uint256 orderId) public view returns(Order memory) {
+    return _getOrder(owner, orderId);
+  }
+
+  /**
+   * @dev This function is used by governors to pre-approve transfers so a spender can
+   *      approve their own transfers.
+   * @param owner is the account from which the transfer is approved from.
+   * @param recipient is the recipient for the pre-approved transfer.
+   * @param maxAmount is how much the transfer can be up to.
+   */
+  function preapprove(
+    address owner,
+    address recipient,
+    uint256 maxAmount
+  ) public governance isActor(owner) isActor(recipient)
+  {
+    Grant[] storage grants = grantBook[owner];
+    grants.push(
+      Grant({
+        approver: msg.sender,
+        owner: owner,
+        recipient: recipient,
+        maxAmount: maxAmount,
+        valid: true
+      })
+    );
+    emit Granted(owner, grants.length - 1);
+  }
+
+  function approveGranted(
+    address owner,
+    uint256 orderId,
+    uint256 grantId
+  ) public isActor(msg.sender) isActor(owner)
+  {
+    // Get the order.
+    Order storage order = _getOrder(owner, orderId);
+    // Get the pre-approval grant.
+    Grant storage grant = _getGrant(owner, grantId);
+    // Ensure that the pre-approval matches the order.
+    require(
+      order.spender == grant.owner,
+      "The specified pre-approval doesn't cover this spender."
+    );
+    require(
+      order.recipient == grant.recipient,
+      "The specified pre-approval doesn't cover this recipient."
+    );
+    require(
+      order.amount <= grant.maxAmount,
+      "The specified pre-approval doesn't cover for this amount."
+    );
+    // Update the pre-approval and render it invalid.
+    invalidateGrant(grant, false);
+    // Update the order status.
+    updateOrder(order, ITransact.Status.Approved);
+    // Make our token contract aware of the changes.
+    reg.token().transferApproved(owner, order.recipient, order.amount);
+    // Emit!
+    emit Approval(owner, orderId);
   }
 
   /**
    * @dev This function approves a transfer. The transfer must be in the `ITransact.Status.Pending` status.
    * @notice For this to work, `msg.sender` must be a governor.
    * @param owner is the account to which the transfer belongs to.
-   * @param id is the identifier that was returned by the `request` function to create the transfer order.
+   * @param orderId is the order ID that was returned by the `request` function to create the transfer order.
    */
-  function approve(address owner, uint256 id) public governance isActor(owner) {
+  function approve(address owner, uint256 orderId) public governance isActor(owner) {
     // Get the order.
-    Order storage order = _get(owner, id);
-    // Update the order.
+    Order storage order = _getOrder(owner, orderId);
+    // Update the order status.
     updateOrder(order, ITransact.Status.Approved);
     // Make our token contract aware of the changes.
     reg.token().transferApproved(owner, order.recipient, order.amount);
     // Emit!
-    emit Approval(owner, id);
+    emit Approval(owner, orderId);
   }
 
   /**
@@ -94,11 +153,11 @@ contract Transact is Initializable, ITransact {
    *      The function can be called either by a governor or the owner of the funds.
    * @notice For this to work, `msg.sender` must be a governor.
    * @param owner is the account to which the transfer belongs to.
-   * @param id is the identifier that was returned by the `request` function to create the transfer order.
+   * @param orderId is the order ID that was returned by the `request` function to create the transfer order.
    */
-  function reject(address owner, uint256 id) public isActor(owner) {
+  function reject(address owner, uint256 orderId) public isActor(owner) {
     // Get the order.
-    Order storage order = _get(owner, id);
+    Order storage order = _getOrder(owner, orderId);
     address spender = order.spender;
     // Make sure that the caller is either a governor, or the owner of the
     // funds, or the person who made the transfer.
@@ -113,15 +172,21 @@ contract Transact is Initializable, ITransact {
     // Make our token contract aware of the changes.
     reg.token().transferRejected(owner, spender, order.amount);
     // Emit!
-    emit Rejection(owner, id);
+    emit Rejection(owner, orderId);
   }
 
   // Private / internal stuff.
 
-  function _get(address owner, uint256 id) internal view returns(Order storage) {
+  function _getOrder(address owner, uint256 orderId) internal view returns(Order storage) {
     Order[] storage orders = orderBook[owner];
-    require(id < orders.length, "The specified order id is invalid");
-    return orders[id];
+    require(orderId < orders.length, "The specified order id is invalid");
+    return orders[orderId];
+  }
+
+  function _getGrant(address owner, uint256 grantId) internal view returns(Grant storage) {
+    Grant[] storage grants = grantBook[owner];
+    require(grantId < grants.length, "The specified pre-approval id is invalid");
+    return grants[grantId];
   }
 
   /**
@@ -135,6 +200,23 @@ contract Transact is Initializable, ITransact {
     require(order.status == ITransact.Status.Pending, "Cannot update a non-pending order");
     // Update the order.
     order.status = status;
+  }
+
+  /**
+   * @dev This function updates a transfer. The transfer must be in the `Status.Pending` status.
+   * @notice For this to work, `msg.sender` must be a governor.
+   * @param order is the order to be updated.
+   * @param status is the new status for the order.
+   */
+  function invalidateGrant(Grant storage grant) internal {
+    // Forbid operating on pending or 0 amount orders.
+    require(
+      grant.valid == true,
+      "The specified pre-approval was already rendered invalid"
+    );
+    // Update the order.
+    grant.valid = false;
+    _;
   }
 
   // Modifiers.
