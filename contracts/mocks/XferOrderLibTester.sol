41aab6dfb4536777ced31202e48e6fe7da27ef5c
Pierre Martin <hickscorp@gmail.com>
2019-11-01 15:11:36 +0000

    Makes Order and Grants libraries.

---
+++ contracts/mocks/XferOrderLibTester.sol
@@ -0,0 +1,44 @@
+pragma solidity ^0.5.9;
+pragma experimental ABIEncoderV2;
+import "../lib/XferOrderLib.sol";
+
+
+contract XferOrderLibTester {
+  using XferOrderLib for XferOrderLib.Data;
+
+  XferOrderLib.Data public sample1;
+
+  /// @dev Proxy to the `XferOrderLib.make` function.
+  function make(address spender, address recipient, uint256 maxAmount) public view returns(XferOrderLib.Data memory) {
+    return XferOrderLib.make(spender, recipient, maxAmount);
+  }
+
+  /// @dev Proxy to the `XferOrderLib.finalize` function.
+  function finalize(XferOrderLib.Status status) public { sample1.finalize(status); }
+
+  /// @dev Proxy to the `XferOrderLib.approve` function.
+  function approve() public { sample1.approve(); }
+
+  /// @dev Proxy to the `XferOrderLib.reject` function.
+  function reject() public { sample1.reject(); }
+
+  // ----------------------------------------------------------------------------- //
+
+  /// @dev Sample 1 setter.
+  function setSample1(
+    address spender,
+    address recipient,
+    uint256 amount,
+    uint256 createdAt,
+    XferOrderLib.Status status) public
+  {
+    sample1 = XferOrderLib.make(spender, recipient, amount);
+    sample1.createdAt = createdAt;
+    sample1.status = status;
+  }
+
+  /// @dev Sample 1 getter.
+  function getSample1() public view returns(XferOrderLib.Data memory) {
+    return sample1;
+  }
+}
---
+++ test/XferOrderLib.test.ts
@@ -0,0 +1,84 @@
+import { XferOrderLibTesterInstance } from '../types/truffle-contracts';
+import { assertNumberEquality, itThrows } from './helpers';
+import { INVALID_ORDER_STATUS } from './errors';
+import { XferOrderStatus } from './constants';
+
+const XferOrderLibTester = artifacts.require('XferOrderLibTester');
+
+contract('XferOrderLib', accounts => {
+  const [_, spender, recipient] = accounts;
+  let tester: XferOrderLibTesterInstance;
+
+  before(async () => {
+    tester = await XferOrderLibTester.new();
+  });
+
+  describe('make', async () => {
+    it('creates an object with its fields set properly', async () => {
+      const sample1 = await tester.make(spender, recipient, '100');
+      assert.equal(sample1.spender, spender);
+      assert.equal(sample1.recipient, recipient);
+      assertNumberEquality(sample1.amount, '100');
+      assertNumberEquality(sample1.status, XferOrderStatus.Pending);
+    });
+  });
+
+  describe('finalize', async () => {
+    itThrows(' already approved', INVALID_ORDER_STATUS, async () => {
+      await tester.setSample1(spender, recipient, '100', '42', XferOrderStatus.Approved);
+      await tester.finalize(XferOrderStatus.Pending);
+    });
+    itThrows(' already rejected', INVALID_ORDER_STATUS, async () => {
+      await tester.setSample1(spender, recipient, '100', '42', XferOrderStatus.Approved);
+      await tester.finalize(XferOrderStatus.Pending);
+    });
+    it('sets status to whatever is passed', async () => {
+      await tester.setSample1(spender, recipient, '100', '42', XferOrderStatus.Pending);
+      const before = await tester.getSample1();
+      assertNumberEquality(before.status, XferOrderStatus.Pending);
+      await tester.finalize(XferOrderStatus.Approved);
+      const after = await tester.getSample1();
+      assertNumberEquality(after.status, XferOrderStatus.Approved);
+    });
+  });
+
+  describe('approve', async () => {
+    itThrows(' already approved', INVALID_ORDER_STATUS, async () => {
+      await tester.setSample1(spender, recipient, '100', '42', XferOrderStatus.Approved);
+      await tester.approve();
+    });
+    itThrows(' already rejected', INVALID_ORDER_STATUS, async () => {
+      await tester.setSample1(spender, recipient, '100', '42', XferOrderStatus.Approved);
+      await tester.approve();
+    });
+
+    it('sets status to approved', async () => {
+      await tester.setSample1(spender, recipient, '100', '42', XferOrderStatus.Pending);
+      const before = await tester.getSample1();
+      assertNumberEquality(before.status, XferOrderStatus.Pending);
+      await tester.approve();
+      const after = await tester.getSample1();
+      assertNumberEquality(after.status, XferOrderStatus.Approved);
+    });
+  });
+
+  describe('reject', async () => {
+    itThrows(' already approved', INVALID_ORDER_STATUS, async () => {
+      await tester.setSample1(spender, recipient, '100', '42', XferOrderStatus.Approved);
+      await tester.reject();
+    });
+    itThrows(' already rejected', INVALID_ORDER_STATUS, async () => {
+      await tester.setSample1(spender, recipient, '100', '42', XferOrderStatus.Approved);
+      await tester.reject();
+    });
+
+    it('sets status to rejected', async () => {
+      await tester.setSample1(spender, recipient, '100', '42', XferOrderStatus.Pending);
+      const before = await tester.getSample1();
+      assertNumberEquality(before.status, XferOrderStatus.Pending);
+      await tester.reject();
+      const after = await tester.getSample1();
+      assertNumberEquality(after.status, XferOrderStatus.Rejected);
+    });
+  });
+});
