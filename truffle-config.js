0e9948dba8f3f1501fdf88aff6f0e019aead70d1
Pierre Martin <hickscorp@gmail.com>
2019-11-01 18:13:10 +0000

    Cleans-up package.json.

--- truffle-config.js
+++ truffle-config.js
@@ -14,21 +14,21 @@ module.exports = {
       port: '7545',
       network_id: '5777',
       gas: 94000000,
-      gas_price: 1
+      gas_price: 0x01
     },
     // Production network.
     cv: {
       provider: () => hdWallet('https://chain.consilienceventures.com'),
       network_id: '18021982',
-      gas: 94000000,
-      gas_price: 1
+      gas: 0x59a5380,
+      gas_price: 0x01
     },
     // Staging network.
     cv_dev: {
       provider: () => hdWallet('https://chain.dev.consilienceventures.com'),
       network_id: '18021981',
       gas: 94000000,
-      gas_price: 1
+      gas_price: 0x01
     }
   },
 
