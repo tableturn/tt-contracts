664a675db19cdad09d169ab830da0d54ebb3896d
Pierre Martin <hickscorp@gmail.com>
2019-11-02 22:43:38 +0000

    Removes unused steps from CI script.

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -10,12 +10,13 @@ npm install yarn -g
 echo "Installing dependencies..."
 yarn
 
-echo "Starting local chain..."
-# ./scripts/ganache.sh > /dev/null &
-
 echo "Building contracts..."
 yarn pretest
+
+# echo "Starting local chain..."
+# ./scripts/ganache.sh > /dev/null &
 # echo "Running tests..."
 # yarn test
+
 echo "Testing and generating coverage..."
 yarn coverage
