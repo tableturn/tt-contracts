d2c39d2440b1920c86d4ddeb4d70df0d9f5b7977
Pierre Martin <hickscorp@gmail.com>
2019-11-03 01:04:28 +0000

    Adds AddressSet and HashSet library tests.

--- test/lib/AddressSetLib.test.ts
+++ test/lib/AddressSetLib.test.ts
@@ -1,12 +1,67 @@
-// TODO!
+import { AddressSetLibTesterInstance } from '../../types/truffle-contracts';
+import { itThrows, assertNumberEquality } from '../helpers/helpers';
+import {
+  ZERO_ADDRESS,
+  INVALID_ZERO_ADDRESS,
+  ADDRESS_ALREADY_IN_SET,
+  NONEXISTENT_ADDRESS
+} from '../helpers/errors';
 
-// Test add.
-// Test add cannot add 0x0.
-// Test add cannot add already added.
+const AddressSetLibTester = artifacts.require('AddressSetLibTester');
 
-// Test remove.
-// Test remove cannot remove not added.
+contract('AddressSetLib', accounts => {
+  const [acc1, acc2, acc3] = accounts;
+  let mock: AddressSetLibTesterInstance;
 
-// Test count.
+  beforeEach(async () => {
+    mock = await AddressSetLibTester.new();
+  });
 
-// Test contains.
+  describe('add', async () => {
+    itThrows('adding the zero address', INVALID_ZERO_ADDRESS, async () => {
+      await mock.add(ZERO_ADDRESS);
+    });
+
+    itThrows('already added', ADDRESS_ALREADY_IN_SET, async () => {
+      await Promise.all([1, 2].map(() => mock.add(acc1)));
+    });
+
+    it('can add properly', async () => {
+      await Promise.all([acc1, acc2, acc3].map(v => mock.add(v)));
+      assert.includeMembers(await mock.getSample1Values(), [acc1, acc2, acc3]);
+    });
+  });
+
+  describe('remove', async () => {
+    itThrows('removing non-existent items', NONEXISTENT_ADDRESS, async () => {
+      await mock.remove(acc1);
+    });
+
+    it('can remove properly', async () => {
+      await Promise.all([acc1, acc2, acc3].map(v => mock.add(v)));
+      assertNumberEquality(await mock.count(), '3');
+      await mock.remove(acc1);
+      assertNumberEquality(await mock.count(), '2');
+      assert.includeMembers(await mock.getSample1Values(), [acc2, acc3]);
+    });
+  });
+
+  describe('count', async () => {
+    it('returns the items count', async () => {
+      await Promise.all([acc1, acc2, acc3].map(v => mock.add(v)));
+      assertNumberEquality(await mock.count(), '3');
+      await mock.remove(acc1);
+      assertNumberEquality(await mock.count(), '2');
+    });
+  });
+
+  describe('contains', async () => {
+    it('functions properly', async () => {
+      assert.equal(await mock.contains(acc1), false);
+      await mock.add(acc1);
+      assert.equal(await mock.contains(acc1), true);
+      await mock.remove(acc1);
+      assert.equal(await mock.contains(acc1), false);
+    });
+  });
+});
--- test/lib/HashSetLib.test.ts
+++ test/lib/HashSetLib.test.ts
@@ -1,11 +1,65 @@
-// TODO!
+import { HashSetLibTesterInstance } from '../../types/truffle-contracts';
+import { itThrows, assertNumberEquality } from '../helpers/helpers';
+import { HASH_ALREADY_IN_SET, NONEXISTENT_HASH } from '../helpers/errors';
 
-// Test add.
-// Test add cannot add already added.
+const HashSetLibTester = artifacts.require('HashSetLibTester');
 
-// Test remove.
-// Test remove cannot remove not added.
+contract('HashSetLib', accounts => {
+  const [acc1, acc2, acc3] = accounts;
+  let mock: HashSetLibTesterInstance;
 
-// Test count.
+  beforeEach(async () => {
+    mock = await HashSetLibTester.new();
+  });
 
-// Test contains.
+  describe('add', async () => {
+    itThrows('already added', HASH_ALREADY_IN_SET, async () => {
+      await Promise.all([1, 2].map(() => mock.add(acc1)));
+    });
+
+    it('can add properly', async () => {
+      await Promise.all([acc1, acc2, acc3].map(v => mock.add(v)));
+      assert.includeMembers(await mock.getSample1Values(), [
+        '0x356cf535fc1f1c4dd1a4c4f4c261684bcd3e1bcb000000000000000000000000',
+        '0x59ef5b8b24ce3e00ad0345cd2d47f96947c3e2c3000000000000000000000000',
+        '0x4b54772cc9e233b4fe1f04a65a994af40a6834ac000000000000000000000000'
+      ]);
+    });
+  });
+
+  describe('remove', async () => {
+    itThrows('removing non-existent items', NONEXISTENT_HASH, async () => {
+      await mock.remove(acc1);
+    });
+
+    it('can remove properly', async () => {
+      await Promise.all([acc1, acc2, acc3].map(v => mock.add(v)));
+      assertNumberEquality(await mock.count(), '3');
+      await mock.remove(acc1);
+      assertNumberEquality(await mock.count(), '2');
+      assert.includeMembers(await mock.getSample1Values(), [
+        '0x4b54772cc9e233b4fe1f04a65a994af40a6834ac000000000000000000000000',
+        '0x59ef5b8b24ce3e00ad0345cd2d47f96947c3e2c3000000000000000000000000'
+      ]);
+    });
+  });
+
+  describe('count', async () => {
+    it('returns the items count', async () => {
+      await Promise.all([acc1, acc2, acc3].map(v => mock.add(v)));
+      assertNumberEquality(await mock.count(), '3');
+      await mock.remove(acc1);
+      assertNumberEquality(await mock.count(), '2');
+    });
+  });
+
+  describe('contains', async () => {
+    it('functions properly', async () => {
+      assert.equal(await mock.contains(acc1), false);
+      await mock.add(acc1);
+      assert.equal(await mock.contains(acc1), true);
+      await mock.remove(acc1);
+      assert.equal(await mock.contains(acc1), false);
+    });
+  });
+});
