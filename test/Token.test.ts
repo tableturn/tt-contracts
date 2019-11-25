0960b243023582a6fe1632f13de939165a5d1d07
Pierre Martin <hickscorp@gmail.com>
2019-11-25 16:28:00 +0000

    Fixes tests to use the new constants.

--- test/Token.test.ts
+++ test/Token.test.ts
@@ -8,15 +8,16 @@ import { itThrows, assertNumberEquality } from './helpers/helpers';
 import {
   ZERO_ADDRESS,
   INSUFFICIENT_FUNDS,
-  MUST_BE_ACTOR,
-  SAME_RECIPIENT,
+  OWNER_SAME_AS_RECIPIENT,
   SELF_ALLOWANCE,
   INSUFFICIENT_ALLOWANCE,
   MUST_BE_TRANSACT,
   DOUBLE_INIT,
   MUST_BE_GOVERNOR,
   MUST_BE_ISSUER,
-  CANNOT_RETRIEVE_FROZEN
+  CANNOT_RETRIEVE_FROZEN,
+  RECIPIENT_NOT_ACTOR,
+  OWNER_NOT_ACTOR
 } from './helpers/errors';
 
 const Registry = artifacts.require('Registry');
@@ -128,7 +129,7 @@ contract('Token', accounts => {
     itThrows('unauthorized issuer', MUST_BE_GOVERNOR, async () => {
       await token.allocate(actor1, '500', issuance);
     });
-    itThrows('recipient is not an actor', MUST_BE_ACTOR, async () => {
+    itThrows('recipient is not an actor', RECIPIENT_NOT_ACTOR, async () => {
       await token.allocate(acc1, '500', governance);
     });
 
@@ -183,16 +184,16 @@ contract('Token', accounts => {
       await token.issue('5000', 'Test issuance', issuance);
     });
 
-    itThrows('sender is not an actor', MUST_BE_ACTOR, async () => {
+    itThrows('sender is not an actor', OWNER_NOT_ACTOR, async () => {
       await token.transfer(actor1, '500', { from: acc1 });
     });
-    itThrows('recipient is not an actor', MUST_BE_ACTOR, async () => {
+    itThrows('recipient is not an actor', RECIPIENT_NOT_ACTOR, async () => {
       await token.transfer(acc1, '500', { from: actor1 });
     });
     itThrows('there are not enough funds', INSUFFICIENT_FUNDS, async () => {
       await token.transfer(actor2, '100', { from: actor1 });
     });
-    itThrows('sender is the same as recipient', SAME_RECIPIENT, async () => {
+    itThrows('sender is the same as recipient', OWNER_SAME_AS_RECIPIENT, async () => {
       await token.allocate(actor1, '100', governance);
       await token.transfer(actor1, '100', { from: actor1 });
     });
@@ -229,16 +230,16 @@ contract('Token', accounts => {
       await token.issue('5000', 'Test issuance', issuance);
     });
 
-    itThrows('owner is not an actor', MUST_BE_ACTOR, async () => {
+    itThrows('owner is not an actor', OWNER_NOT_ACTOR, async () => {
       await token.transferFrom(acc1, actor3, '100', { from: actor1 });
     });
-    itThrows('recipient is not an actor', MUST_BE_ACTOR, async () => {
+    itThrows('recipient is not an actor', RECIPIENT_NOT_ACTOR, async () => {
       await token.transferFrom(actor2, acc2, '100', { from: actor1 });
     });
     itThrows('owner and spender are the same', SELF_ALLOWANCE, async () => {
       await token.transferFrom(actor1, actor2, '100', { from: actor1 });
     });
-    itThrows('sender is the same as recipient', SAME_RECIPIENT, async () => {
+    itThrows('sender is the same as recipient', OWNER_SAME_AS_RECIPIENT, async () => {
       await token.transferFrom(actor2, actor2, '100', { from: actor1 });
     });
     itThrows('allowance is insuficient', INSUFFICIENT_ALLOWANCE, async () => {
@@ -365,10 +366,10 @@ contract('Token', accounts => {
     itThrows('called as issuer', MUST_BE_GOVERNOR, async () => {
       await token.retrieveDeadTokens(actor1, actor2, issuance);
     });
-    itThrows('recipient is not an actor', MUST_BE_ACTOR, async () => {
+    itThrows('recipient is not an actor', RECIPIENT_NOT_ACTOR, async () => {
       await token.retrieveDeadTokens(actor1, acc1, governance);
     });
-    itThrows('owner is not an actor', MUST_BE_ACTOR, async () => {
+    itThrows('owner is not an actor', OWNER_NOT_ACTOR, async () => {
       await token.retrieveDeadTokens(acc1, actor2, governance);
     });
     describe('when there are tokens to retrieve', () => {
