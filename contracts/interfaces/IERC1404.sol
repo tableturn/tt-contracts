5a293e526f6c3b4991e83b96a5b7a2c9e3e1df71
Pierre Martin <hickscorp@gmail.com>
2019-11-25 15:43:58 +0000

    Creates an ERC1404 interface file.

---
+++ contracts/interfaces/IERC1404.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.5.9;
+
+
+/**
+ * @dev Interface of the ERC1404 standard as defined in the EIP.
+ */
+interface IERC1404 {
+  /**
+   * @dev Returns a transfer restriction error code if the transfer shoudln't be permitted,
+   *      or otherwise returns 0 if everything looks fine.
+   * @param owner is the account from which the tokens should be transfered.
+   * @param recipient is the target of the transfer.
+   * @param amount is the amount to be transfered.
+   * @return a uint8 value.
+   */
+  function detectTransferRestriction (address owner, address recipient, uint256 value)
+    external view returns (uint8);
+
+  /**
+   * @dev Maps a transfer restriction error code into a human-readable string.
+   * @param code is the code that should be transformed into a string.
+   * @return A string that describes the given error code.
+   */
+  function messageForTransferRestriction (uint8 code)
+    external view returns (string memory);
+}
