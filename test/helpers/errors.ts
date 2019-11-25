8e9b58f8e8b09164fe89c09c75a11a8c65953c6f
Pierre Martin <hickscorp@gmail.com>
2019-11-25 16:25:00 +0000

    Adds required error messages in the test constants.

--- test/helpers/errors.ts
+++ test/helpers/errors.ts
@@ -2,7 +2,10 @@ export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
 export const DOUBLE_INIT = 'Contract instance has already been initialized';
 export const MUST_BE_ISSUER = 'This function must be called by an issuer';
 export const MUST_BE_ACTOR = 'Provided account is not an actor';
+export const OWNER_NOT_ACTOR = 'Owner of funds must be an actor';
+export const RECIPIENT_NOT_ACTOR = 'Recipient of funds must be an actor';
 export const MUST_BE_GOVERNOR = 'This function must be called by a governor';
+export const OWNER_SAME_AS_RECIPIENT = 'Recipient cannot be the same as owner';
 export const INVALID_ORDER_ID = 'The specified order id is invalid';
 export const INVALID_ORDER_STATUS = 'Cannot update a non-pending order';
 export const INVALID_GRANT_ID = 'The specified grant id is invalid';
@@ -24,7 +27,6 @@ export const INSUFFICIENT_FUNDS = 'Insufficient funds';
 export const INSUFFICIENT_FROZEN_FUNDS = 'Insufficient frozen funds';
 export const INSUFFICIENT_ALLOWANCE = 'Insufficient allowance from owner';
 
-export const SAME_RECIPIENT = 'Recipient cannot be the same as owner';
 export const SELF_ALLOWANCE = 'Cannot perform a transfer using allowance on behalf of yourself';
 
 export const DUPLICATED_ADDRESS = 'Address already in set';
