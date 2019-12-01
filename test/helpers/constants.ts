08820dc426ed1aa1c84fd184907d97a7e15e20be
Pierre Martin <hickscorp@gmail.com>
2019-12-01 17:58:29 +0000

    Improves constants and test helpers.

--- test/helpers/constants.ts
+++ test/helpers/constants.ts
@@ -1,12 +1,19 @@
 import BN = require('bn.js');
 
+export const ZERO = new BN(0);
+export const ONE = new BN(1);
+export const TWO = new BN(2);
+export const THREE = new BN(3);
+
+export const BAD_ID = '0x0000000000000000000000000000000000000000000000000000000000000000';
+
 export const XferGrantStatus = {
-  Valid: new BN(0),
-  Used: new BN(1)
+  Valid: ZERO,
+  Used: ONE
 };
 
 export const XferOrderStatus = {
-  Pending: new BN(0),
-  Approved: new BN(1),
-  Rejected: new BN(2)
+  Pending: ZERO,
+  Approved: ONE,
+  Rejected: TWO
 };
--- test/helpers/errors.ts
+++ test/helpers/errors.ts
@@ -1,4 +1,5 @@
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+
 export const DOUBLE_INIT = 'Contract instance has already been initialized';
 export const MUST_BE_ISSUER = 'This function must be called by an issuer';
 export const MUST_BE_ACTOR = 'Provided account is not an actor';
@@ -6,34 +7,29 @@ export const OWNER_NOT_ACTOR = 'Owner of funds must be an actor';
 export const RECIPIENT_NOT_ACTOR = 'Recipient of funds must be an actor';
 export const MUST_BE_GOVERNOR = 'This function must be called by a governor';
 export const OWNER_SAME_AS_RECIPIENT = 'Recipient cannot be the same as owner';
-export const INVALID_ORDER_ID = 'The specified order id is invalid';
+export const INVALID_ORDER_INDEX = 'The specified order index is invalid';
+export const INVALID_ORDER = 'The specified order is invalid';
 export const INVALID_ORDER_STATUS = 'Cannot update a non-pending order';
-export const INVALID_GRANT_ID = 'The specified grant id is invalid';
+export const INVALID_GRANT_INDEX = 'The specified grant index is invalid';
+export const INVALID_GRANT = 'The specified grant is invalid';
 export const INVALID_GRANT_STATUS = 'Cannot redeem an already invalid grant';
+export const GRANT_OWNER_MISMATCH = 'The specified order and grant must belong to you';
 export const GRANT_RECIPIENT_MISMATCH = "The specified pre-approval doesn't cover this recipient";
 export const GRANT_AMOUNT_MISMATCH = "The specified pre-approval doesn't cover for this amount";
-
 export const MUST_BE_TRANSACT = 'This function can only be called by the Transact contract';
 export const MUST_BE_TOKEN = 'This function can only be called by the Token contract';
-
 export const INVALID_ZERO_ADDRESS = 'Address cannot be 0x0';
 export const ADDRESS_ALREADY_IN_SET = 'Address already in set';
 export const NONEXISTENT_ADDRESS = 'Address does not exist in set';
-
 export const HASH_ALREADY_IN_SET = 'Hash already in set';
 export const NONEXISTENT_HASH = 'Hash does not exist in set';
-
 export const INSUFFICIENT_FUNDS = 'Insufficient funds';
 export const INSUFFICIENT_FROZEN_FUNDS = 'Insufficient frozen funds';
 export const INSUFFICIENT_ALLOWANCE = 'Insufficient allowance from owner';
-
 export const SELF_ALLOWANCE = 'Cannot perform a transfer using allowance on behalf of yourself';
-
 export const DUPLICATED_ADDRESS = 'Address already in set';
 export const DUPLICATED_HASH = 'Hash already in set';
 export const SELF_TERMINATION = 'Cannot self-destruct as a governor';
 export const ADDITION_OVERFLOW = 'SafeMath: addition overflow';
-
 export const CANNOT_RETRIEVE_FROZEN = 'Cannot retrieve dead tokens on an account with frozen funds';
-
 export const UNKNOWN_ERC1404_CODE = 'Unknown transfer restriction error code';
