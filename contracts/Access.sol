9075d52f60bca8e1afc25d9aa5e6213dcf3cb155
Pierre Martin <hickscorp@gmail.com>
2020-03-04 13:43:13 +0000

    Lints a few files.

--- contracts/Access.sol
+++ contracts/Access.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.5.9;
-import "@openzeppelin/upgrades/contracts/Initializable.sol";
-import "./interfaces/IAccess.sol";
-import "./lib/AddressSetLib.sol";
+pragma experimental ABIEncoderV2;
 
+import '@openzeppelin/upgrades/contracts/Initializable.sol';
+import './interfaces/IAccess.sol';
+import './lib/AddressSetLib.sol';
 
 /**
 contract * @title Access
@@ -40,7 +41,7 @@ contract Access is Initializable, IAccess {
    * @dev Retrieves the list of issuers.
    * @return the list of addresses considered issuers.
    */
-  function issuers() external view returns(address[] memory) {
+  function issuers() external view returns (address[] memory) {
     return issuerList.values;
   }
 
@@ -49,7 +50,7 @@ contract Access is Initializable, IAccess {
    * @param c is an address to test for issuance belonging.
    * @return a boolean.
    */
-  function isIssuer(address c) external view returns(bool) {
+  function isIssuer(address c) public view returns (bool) {
     return issuerList.contains(c);
   }
 
@@ -57,7 +58,7 @@ contract Access is Initializable, IAccess {
    * @dev Adds an address to the list of issuers.
    * @param a is the address to be added as issuer.
    */
-  function addIssuer(address a) external governance {
+  function addIssuer(address a) public governance {
     issuerList.add(a);
     emit IssuerAdded(a);
   }
@@ -66,7 +67,7 @@ contract Access is Initializable, IAccess {
    * @dev Removes an issuer from the issuers list.
    * @param a is the address of the issuer to be removed.
    */
-  function removeIssuer(address a) external governance {
+  function removeIssuer(address a) public governance {
     issuerList.remove(a);
     emit IssuerRemoved(a);
   }
@@ -77,7 +78,7 @@ contract Access is Initializable, IAccess {
    * @dev Retrieves the list of governors.
    * @return the list of addresses considered governors.
    */
-  function governors() external view returns(address[] memory) {
+  function governors() external view returns (address[] memory) {
     return governorList.values;
   }
 
@@ -86,7 +87,7 @@ contract Access is Initializable, IAccess {
    * @param c is an address to test for governance belonging.
    * @return a boolean.
    */
-  function isGovernor(address c) external view returns(bool) {
+  function isGovernor(address c) public view returns (bool) {
     return governorList.contains(c);
   }
 
@@ -94,7 +95,7 @@ contract Access is Initializable, IAccess {
    * @dev Adds an address to the governance list.
    * @param a is the address to be added as governor.
    */
-  function addGovernor(address a) external governance {
+  function addGovernor(address a) public governance {
     governorList.add(a);
     emit GovernorAdded(a);
   }
@@ -103,11 +104,8 @@ contract Access is Initializable, IAccess {
    * @dev Removes a governor from the governance list.
    * @param a is the address of the governor to be removed.
    */
-  function removeGovernor(address a) external governance {
-    require(
-      msg.sender != a,
-      "Cannot self-destruct as a governor"
-    );
+  function removeGovernor(address a) public governance {
+    require(msg.sender != a, 'Cannot self-destruct as a governor');
     governorList.remove(a);
     emit GovernorRemoved(a);
   }
@@ -118,7 +116,7 @@ contract Access is Initializable, IAccess {
    * @dev Retrieves the list of actors.
    * @return the list of addresses considered actors.
    */
-  function actors() external view returns(address[] memory) {
+  function actors() external view returns (address[] memory) {
     return actorList.values;
   }
 
@@ -127,7 +125,7 @@ contract Access is Initializable, IAccess {
    * @param c is an address to test for acting belonging.
    * @return a boolean.
    */
-  function isActor(address c) external view returns(bool) {
+  function isActor(address c) public view returns (bool) {
     return actorList.contains(c);
   }
 
@@ -135,7 +133,7 @@ contract Access is Initializable, IAccess {
    * @dev Adds an address to the actors list.
    * @param a is the address to be added as an actor.
    */
-  function addActor(address a) external governance {
+  function addActor(address a) public governance {
     actorList.add(a);
     emit ActorAdded(a);
   }
@@ -144,7 +142,7 @@ contract Access is Initializable, IAccess {
    * @dev Removes an actor from the actors list.
    * @param a is the address of the actor to be removed.
    */
-  function removeActor(address a) external governance {
+  function removeActor(address a) public governance {
     actorList.remove(a);
     emit ActorRemoved(a);
   }
@@ -152,10 +150,7 @@ contract Access is Initializable, IAccess {
   // Modifiers.
 
   modifier governance {
-    require(
-      governorList.contains(msg.sender),
-      "This function must be called by a governor"
-    );
+    require(governorList.contains(msg.sender), 'This function must be called by a governor');
     _;
   }
 }
