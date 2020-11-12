c2b628f680ac37192ef9aba22dd268482c2229d2
Pierre Martin <hickscorp@gmail.com>
2020-11-12 19:30:18 +0000

    Starts a local ganache as we're not using solcover anymore.

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -13,6 +13,11 @@ yarn
 echo "Building contracts..."
 yarn pretest
 
+echo "Starting local chain..."
+./scripts/ganache.sh > /dev/null &
+echo "Running tests..."
+yarn test
+
 echo "Testing and generating coverage..."
 # yarn coverage
 yarn test
