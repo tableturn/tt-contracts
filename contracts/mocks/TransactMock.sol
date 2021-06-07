05ea8573c11c1237a73b008abcf6c85103372502
Pierre Martin <hickscorp@gmail.com>
2021-06-07 18:24:04 +0200

    Adds the ability for transfers to receive a reference string.

--- contracts/mocks/TransactMock.sol
+++ contracts/mocks/TransactMock.sol
@@ -2,19 +2,23 @@ pragma solidity ^0.5.9;
 pragma experimental ABIEncoderV2;
 import '../interfaces/ITransact.sol';
 
-
 contract TransactMock is ITransact {
   struct CallProof {
     address owner;
     address spender;
     address recipient;
     uint256 amount;
+    string ref;
   }
   CallProof[] private requests;
 
+
+  address private constant ZERO_ADDRESS = address(0);
+  bytes32 private constant ZERO_ORDER_ID = "00000000000000000000000000000000";
+
   /// @dev Mocks to the `ITransact.request` function.
-  function request(address owner, address spender, address recipient, uint256 amount) external {
-    requests.push(CallProof(owner, spender, recipient, amount));
+  function request(address owner, address spender, address recipient, uint256 amount, string calldata ref) external {
+    requests.push(CallProof(owner, spender, recipient, amount, ref));
   }
 
   // ----------------------------------------------------------------------------- //
