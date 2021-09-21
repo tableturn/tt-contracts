6fb8d6317f0e0cac5d3e583e245700cb9818f142
Pierre Martin <hickscorp@gmail.com>
2021-09-21 13:01:18 +0200

    Tweaks configuration to ignore gas specification on all networks.

--- truffle-config.js
+++ truffle-config.js
@@ -14,30 +14,32 @@ module.exports = {
       host: 'localhost',
       port: '7545',
       network_id: '5777',
-      gas: "0x59a5380",
-      gas_price: 0x01
+      // gas: "0x59a5380",
+      // gas_price: 0x01
     },
     // Coverage network config.
     coverage: {
       host: 'localhost',
       network_id: '*',
       port: 8555,
-      gas: "0xfffffffffff",
-      gasPrice: 0x01
+      // gas: "0xfffffffffff",
+      // gasPrice: 0x01
     },
     // Production network.
     cv: {
       provider: () => hdWallet('https://chain.consilienceventures.com'),
       network_id: '18021982',
-      gas: "0x59a5380",
-      gas_price: 0x01
+      // Find using `web3.eth.getBlock("pending")`.
+      // gas: "0x598ecec",
+      // gas_price: 1
     },
     // Staging network.
     cv_dev: {
       provider: () => hdWallet('https://chain.dev.consilienceventures.com'),
       network_id: '18021981',
-      gas: "0x59a5380",
-      gas_price: 0x01
+      // Find using `web3.eth.getBlock("pending")`.
+      // gas: "0x59a5380",
+      // gas_price: 1
     }
   },
 
