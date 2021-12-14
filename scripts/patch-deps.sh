cfd3c3ec2cb76ede4bb3030cd3f0f9f64753dfed
Pierre Martin <hickscorp@gmail.com>
2021-12-14 20:07:27 +0100

    Temporarily fixes a build script for frontend.

--- scripts/patch-deps.sh
+++ scripts/patch-deps.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+NODE_MODULES="$(pwd)/.."
 deps=("@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/IERC20.sol" "@openzeppelin/upgrades/contracts/Initializable.sol")
 
 for dep in "${deps[@]}"; do
   echo "Patching ${dep} ..."
-  sed -i 's/^pragma solidity.*;/pragma solidity ^0.8.0;/' "node_modules/$dep"
+  sed -i 's/^pragma solidity.*;/pragma solidity ^0.8.0;/' "${NODE_MODULES}/$dep"
 done
